48989cb1e6ab08d29f5c090f072996c8e94d1e72
RummelBummel <yonasleschber@gmail.com>
2026-01-19 22:56:46 +0100

    a

--- de/seuhd/observer/ConcreteObserver.java
+++ de/seuhd/observer/ConcreteObserver.java
@@ -2,17 +2,30 @@ package de.seuhd.observer;
 
 public class ConcreteObserver implements Observer {
     // TODO: Add fields and static variables.
+    private static int nextId = 1;
+
+    private final int id;
+    private final Subject subject;
 
     public ConcreteObserver(Subject subject) {
         // TODO: Implement constructor.
+        this.subject = subject;
+        this.id = nextId++;
+        this.subject.attach(this);
     }
 
     @Override
     public void update(Subject subject) {
         // TODO: Implement update method.
+        if (subject instanceof ConcreteSubject concreteSubject) {
+            System.out.println(
+                    "Observer " + id + " received update from subject : New value is " + concreteSubject.getValue()
+            );
+        }
     }
 
     public void detach() {
         // TODO: Implement detach method.
+        subject.detach(this);
     }
 }
--- de/seuhd/observer/Subject.java
+++ de/seuhd/observer/Subject.java
@@ -1,17 +1,32 @@
 package de.seuhd.observer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class Subject {
     // TODO: Add fields.
+    private final List<Observer> observers = new ArrayList<>();
 
     void attach(Observer observer) {
         // TODO: Implement attach method.
+        if (observer == null) return;
+        if (!observers.contains(observer)) {
+            observers.add(observer);
+        }
     }
 
     void detach(Observer observer) {
         // TODO: Implement detach method.
+        observers.remove(observer);
     }
 
     protected void notifyObservers() {
         // TODO: Implement fireUpdate method.
+        System.out.println("Sending update to observers ...");
+        // defensive copy: erlaubt detach() während update ohne ConcurrentModification
+        List<Observer> snapshot = new ArrayList<>(observers);
+        for (Observer o : snapshot) {
+            o.update(this);
+        }
     }
 }
